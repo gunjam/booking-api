8343ba150fe741158bcb0bb297a43a9341daf367
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-02-16 16:25:14 +0000

    Add validaiton to prevent double bookings

--- common/models/booking.js
+++ common/models/booking.js
@@ -1,5 +1,33 @@
 'use strict';
 
-module.exports = function(Booking) {
+// {start: {between: [start, end]}},
 
+module.exports = function(Booking) {
+  Booking.validateAsync('doubleBooking', checkBookings, {
+    message: 'Can\'t book this, it would be a double booking'
+  });
+
+  function checkBookings(err, done) {
+    const {start, end, roomId} = this;
+
+    Booking.find({
+      where: {
+        and: [
+          {roomId},
+          {or: [
+            {and: [{start: {lte: start}}, {end: {gt: start}}]},
+            {and: [{start: {lt: end}}, {end: {gte: end}}]},
+            {and: [{start: start, end: end}]}
+          ]}
+        ]
+      }
+    })
+    .then(bookings => {
+      if (bookings.length > 0) {
+        err();
+      }
+      done();
+    })
+    .catch(err);
+  }
 };
